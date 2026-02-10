8c1be39a6cfdb407b020b0e3b02be4c57c55fbd1
bilal <bilalshaikh1427@gmail.com>
2026-02-10 12:23:19 +0530

    Add environment configs for API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: '/api'
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // Add token to requests
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
